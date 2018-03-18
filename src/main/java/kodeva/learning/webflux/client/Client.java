9b83e9fa2d1f4b01ab674fee9400e75918112d39
Stepan Hrbacek <stepan.hrbacek@solarwinds.com>
2018-03-18 20:05:16 +0100

    Add async invocation of client

--- src/main/java/kodeva/learning/webflux/client/Client.java
+++ src/main/java/kodeva/learning/webflux/client/Client.java
@@ -13,7 +13,10 @@ public class Client {
             .accept(MediaType.TEXT_PLAIN)
             .exchange();
 
-    public String getResult() {
-        return ">> result = " + result.flatMap(res -> res.bodyToMono(String.class)).block();
+    public void testRequest() {
+        System.out.println("... Launching test request");
+        Mono<String> mono = result.flatMap(res -> res.bodyToMono(String.class));
+        mono.subscribe(s -> System.out.format("...... Response received: '%s'", s));
+        System.out.println("... Test request launched");
     }
 }
